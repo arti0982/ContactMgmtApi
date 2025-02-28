d0355688cf3a67dcbfdf3c7a6d90f193c1b9bd63
artidesai <arti0982@gmail.com>
2025-02-28 14:06:19 +1100

    Base commit

---
+++ src/main/java/com/belong/contactmgmtapi/ContactMgmtApiApplication.java
@@ -0,0 +1,13 @@
+package com.belong.contactmgmtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMgmtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactMgmtApiApplication.class, args);
+    }
+
+}
